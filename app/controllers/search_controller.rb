75e17dd4d606d753d134b0ad16a3bfcec6ed3457
carolyoon <carolyoon731@gmail.com>
2018-02-14 16:28:58 -0800

    Create attendance model and migration

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,7 @@
 class SearchController < ApplicationController
 
   def index
-    p @date = DateTime.now.to_s[0..-7]
-    @events = SearchHelper.event_search("Oakland", "music", DateTime.now - 1.week)
-    
+    @events = SearchHelper.event_search("Oakland", "music", DateTime.now - 1.week)   
   end
 
 end
