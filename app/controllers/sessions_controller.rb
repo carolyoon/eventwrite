4c7ce234665e3b4c64978f7be85b66e4b33f1cc0
carolyoon <carolyoon731@gmail.com>
2018-02-15 18:33:49 -0800

    Make sessions create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
-  def new
+  include SessionsHelper
+
+  def create
+    @user = User.find_by(email: session_params[:email])
+    if @user && @user.authenticate(session_params[:password])
+      redirect_to root_path
+    else
+      redirect_to root_path, :notice => "Invalid email or password."
+    end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def session_params
+  end
+
 end
