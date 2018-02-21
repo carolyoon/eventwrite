79ca7c414b257ca664500f91d4873eb89b3ecf91
carolyoon <carolyoon731@gmail.com>
2018-02-21 15:36:20 -0800

    Change homepage to register form

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:create, :destroy]
 
-  root 'search#index'
+  root 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
