119e45d193a7e53f10eb4dd5e59ccdb1c65d6a07
carolyoon <carolyoon731@gmail.com>
2018-02-22 23:21:08 -0800

    Create link to go to attendance show after creating attendance object

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,13 +2,23 @@ class AttendancesController < ApplicationController
   include SessionsHelper
 
   def create
-    p "*" * 80
-    p params
-    @attendance = current_user.attendances.new(event_id: params[:event_id])
-    if @attendance.save
-      redirect_to @attendance
+    if attended?
+      flash[:notice]= "You already saved this event."
+      redirect_back(fallback_location: events_path)
     else
-      redirect_back 
+      @attendance = current_user.attendances.new(event_id: params[:event_id])
+      if @attendance.save
+        redirect_to @attendance
+      else
+        redirect_back 
+      end
     end
   end
+
+  private
+
+    def attended?
+      Attendance.find_by(user: current_user, event_id: params[:event_id]) 
+    end
+
 end
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,6 @@
 module AttendancesHelper
+  def find_attendance(user_id, event_id)
+    @attendance = Attendance.find_by(user_id: user_id, event_id: event_id)
+    @attendance.id
+  end
 end
